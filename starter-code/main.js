8cf0ba9e5dc2c32c10142332083abc56593e5176
Adam Hoff <adamh@Adams-MacBook-Pro.local>
2017-03-15 19:14:35 -0400

    Updated card elements to JavaScript

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,12 +1,14 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-if (cardOne === cardTwo) {
-	alert('You found a match!');
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [ ];
+var gameBoard = document.getElementById('game-board');
+
+function createCards() {
+	for (var i=0; i < cards.length; i ++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		gameBoard.appendChild(cardElement);
+	}
 }
-else {
-	alert('Sorry, try again.');
-}
+createCards();
